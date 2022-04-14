5485618d157017baa1bd97be4d0e9476cc872fac
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-04-14 14:57:50 +0300

    eslint errors fixed

---
+++ src/bin/index.js
@@ -0,0 +1,20 @@
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .name('genDiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
+
+program.command('split')
+  .description('Split a string into substrings and display as an array')
+  .argument('<string>', 'string to split')
+  .option('--first', 'display just the first substring')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
+
+program.parse();
---
+++ src/gendiff.js
@@ -0,0 +1,16 @@
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
+
+program.command('split')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
+
+program.parse();
