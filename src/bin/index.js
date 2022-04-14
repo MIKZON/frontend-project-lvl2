f454aec574d93eb717479442cd1271f3beebd689
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-04-14 15:32:46 +0300

    added gendiff test

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,20 +1,5 @@
-const { Command } = require('commander');
+import genDiff from '@hexlet/code';
 
-const program = new Command();
+const diff = genDiff(filepath1, filepath2);
 
-program
-  .name('genDiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
-
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
-
-program.parse();
+console.log(diff);
