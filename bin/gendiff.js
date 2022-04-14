bbdc093404052b2c94c03121afe3835192fd240c
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-04-14 17:22:11 +0300

    added options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
-program.command('split')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+
 
 program.parse();
