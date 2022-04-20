38c84acdc89b9c80abd626ac5a3cb146dcd78ee7
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-04-20 15:32:48 +0300

    added 3step

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import fs from 'fs';
+import fs, { read } from 'fs';
 import path from 'path';
 
 const genDiff = (filepath1, filepath2) => {
@@ -15,15 +15,18 @@ const genDiff = (filepath1, filepath2) => {
   const allKeys = [...keysOfFirstFile, ...keysOfSecondFile];
   const allKeysUniq = _.uniq(allKeys).sort();
 
-  allKeysUniq.map((key) => {
-
-    // const file1Value = _.has(readFirstFile, key)
-    // const file2Value = _.has(readSecondFile, key)
-    // if (readFirstFile[key] && !readSecondFile[key]) { console.log( `- ${key}: ${readFirstFile[key]}`) }
-    // if (readFirstFile[key] && readSecondFile[key]) {console.log( ` - ${key}: ${readFirstFile[key]}`)}
-    // if (!readFirstFile[key] && !readSecondFile[key]) { console.log (`- ${key}: ${readFirstFile[key]}`)}
-    // if (file1Value && file2Value  || readFirstFile[key] === readSecondFile[key]) {console.log(`+ ${key}: ${readSecondFile[key]}`)}
+  const lines = allKeysUniq.map((key) => {
+    const file1Value = _.has(readFirstFile, key);
+    const file2Value = _.has(readSecondFile, key);
+    if (file1Value && !file2Value) { return `- ${key}: ${readFirstFile[key]}`}
+    if (!file1Value && file2Value) { return `+ ${key}: ${readSecondFile[key]}`}
+    if (file1Value && file2Value && readFirstFile[key] === readSecondFile[key]) { return `  ${key}: ${readSecondFile[key]}`}
+    // if (readFirstFile[key]) { return `- ${key}: ${readFirstFile[key]}`};
+    // if (readSecondFile[key]) { return `+ ${key}: ${readSecondFile[key]}`};
+    if (readFirstFile[key] || readSecondFile[key])
+    return `- ${key}: ${readFirstFile[key]} \n+ ${key}: ${readSecondFile[key]}`;
   });
+  return ['{', ...lines, '}'].join('\n');
 };
 
 export default genDiff;
