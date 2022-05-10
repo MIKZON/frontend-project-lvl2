e0e787ea2a257cb04d31b3ae85a6490e7fce399b
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-05-10 18:15:12 +0300

    added test for genDiff

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,18 @@
 import fs from 'fs';
-import path from 'path';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test(('firstCase'), () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  expect(getFixturePath).toBe('string')
-})
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
+
+const afterJson = getFixturePath('after.json');
+const beforeJson = getFixturePath('before.json');
+
+test(('genDiff testCase'), () => {
+  const resultPath = readFile(getFixturePath('result.txt', 'utf-8'));
+  expect(genDiff(afterJson, beforeJson)).toEqual(resultPath);
+});
