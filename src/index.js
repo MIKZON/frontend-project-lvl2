61f6f23d2442b7cb4519d3d6b5e1c011b12592ab
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-05-26 10:42:32 +0300

    fixed parse errors

--- src/index.js
+++ src/index.js
@@ -3,26 +3,15 @@ import _ from 'lodash';
 import parse from './ parsers/parser.js';
 
 const genDiff = (filepath1, filepath2) => {
-  // const getPathFirstFile = getFixturePath(filepath1);
-  // let parse;
-  // if (format === '.json' || format === '') {
-  //   parse = JSON.parse;
-  // } else if (format === '.yml' || format === '.yaml') {
-  //   parse = yaml.load;
-  // }
   const parseFirstFile = parse(filepath1);
   const parseSecondFile = parse(filepath2);
-  // const readFirstFile = parse(readFile(getPathFirstFile, 'utf-8'));
-
-  // const getPathSecondFile = path.resolve(process.cwd(), filepath2);
-  // const readSecondFile = parse(fs.readFileSync(getPathSecondFile, 'utf-8'));
 
   const keysOfFirstFile = Object.keys(parseFirstFile);
-  const keysOfSecondFile = Object.keys(parse(filepath2));
+  const keysOfSecondFile = Object.keys(parseSecondFile);
 
   const allKeys = [...keysOfFirstFile, ...keysOfSecondFile];
-
   const allKeysUniq = _.uniq(allKeys).sort();
+
   const lines = allKeysUniq.map((key) => {
     const file1Value = _.has((parseFirstFile), key);
     const file2Value = _.has((parseSecondFile), key);
