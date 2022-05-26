a80177337fcfc6435b8ec0ece476da71223229e1
Magomed Ilyasov <mkznworkflow@gmail.com>
2022-05-26 10:43:01 +0300

    fixed tests errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,11 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { getFixturePath, readFile } from '../src/ parsers/filepath.js';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const afterJson = ('after.json');
+const beforeJson = ('before.json');
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
-
-const afterJson = getFixturePath('after.json');
-const beforeJson = getFixturePath('before.json');
-
-const afterYaml = getFixturePath('after.yml');
-const beforeYaml = getFixturePath('before.yml');
+const afterYaml = ('after.yml');
+const beforeYaml = ('before.yml');
 
 test(('genDiff testCase'), () => {
   const resultPath = readFile(getFixturePath('result.txt', 'utf-8'));
